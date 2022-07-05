cde7d3a1821ea4ffee84d3ce667492423e4d80bd
Erick Rosdachimo <erickrosdachimoribeiro@gmail.com>
2022-07-05 10:50:42 -0300

    adicionando a função ver menos, porém ainda esta incompleta

--- menu/menu.js
+++ menu/menu.js
@@ -46,163 +46,94 @@ var pratos = [
   }
 ]
 var boxAdd = 0
+var boxVisiveis = 0
 var pratosUsados = 0
 var imgCarrossel = 0
+
 function adicionar() {
-  const containerSobre = document.getElementById('container-sobre')
-
-  // criando o separador
-  const divisor = document.createElement('img')
-
-  // --- editando o separador
-  divisor.setAttribute('src', '../assets/images/divisor.png')
-  divisor.classList.add('divisor-icon')
-
-  // ------ adicionando o separador
-  containerSobre.appendChild(divisor)
-
-  // criando as bases
-  const contMargin = document.createElement('div')
-  const boxPratos = document.createElement('div')
-  const boxG = document.createElement('div')
-
-  // --- editando as bases
-  contMargin.classList.add('container-margin')
-  boxPratos.classList.add('box-pratos')
-  boxG.classList.add('carrossel-cardapio')
-
-  // ------- adicionando as bases
-  containerSobre.appendChild(contMargin)
-  contMargin.appendChild(boxPratos)
-  contMargin.appendChild(boxG)
-
-  // criando elementos para o prato
-  const prato1 = document.createElement('div')
-  const prato2 = document.createElement('div')
-  const prato3 = document.createElement('div')
-  const pratoImg1 = document.createElement('img')
-  const pratoImg2 = document.createElement('img')
-  const pratoImg3 = document.createElement('img')
-  const pratoSpan1 = document.createElement('span')
-  const pratoSpan2 = document.createElement('span')
-  const pratoSpan3 = document.createElement('span')
-  const pratoNome1 = document.createElement('p')
-  const pratoNome2 = document.createElement('p')
-  const pratoNome3 = document.createElement('p')
-  const pratoDesc1 = document.createElement('p')
-  const pratoDesc2 = document.createElement('p')
-  const pratoDesc3 = document.createElement('p')
-
-  // --- editando e adicionando elementos para o prato
-
-  var spanRegua1 = document.createElement('span')
-  spanRegua1.classList.add('regua')
-  var spanRegua2 = document.createElement('span')
-  spanRegua2.classList.add('regua')
-  var spanRegua3 = document.createElement('span')
-  spanRegua3.classList.add('regua')
-  // prato 1
-  pratoDesc1.textContent = pratos[pratosUsados].desc
-  pratoDesc1.classList.add('desc')
-  pratoNome1.textContent = pratos[pratosUsados].nome
-  pratoNome1.classList.add('nome')
-  pratoImg1.setAttribute('src', pratos[pratosUsados].img)
-
-  prato1.classList.add('prato')
-
-  prato1.appendChild(pratoImg1)
-  pratoSpan1.appendChild(pratoNome1)
-  pratoSpan1.appendChild(pratoDesc1)
-  prato1.appendChild(pratoSpan1)
-
-  boxPratos.appendChild(prato1)
-
-  pratosUsados = pratosUsados + 1
-
-  boxPratos.appendChild(spanRegua1)
-
-  // prato 2
-  pratoDesc2.textContent = pratos[pratosUsados].desc
-  pratoDesc2.classList.add('desc')
-  pratoNome2.textContent = pratos[pratosUsados].nome
-  pratoNome2.classList.add('nome')
-  pratoImg2.setAttribute('src', pratos[pratosUsados].img)
-  prato2.classList.add('prato')
-
-  pratoSpan2.appendChild(pratoNome2)
-  pratoSpan2.appendChild(pratoDesc2)
-  prato2.appendChild(pratoImg2)
-  prato2.appendChild(pratoSpan2)
-
-  boxPratos.appendChild(prato2)
-
-  pratosUsados = pratosUsados + 1
-  boxPratos.appendChild(spanRegua2)
-
-  // prato 3
-  pratoDesc3.textContent = pratos[pratosUsados].desc
-  pratoDesc3.classList.add('desc')
-  pratoNome3.textContent = pratos[pratosUsados].nome
-  pratoNome3.classList.add('nome')
-  pratoImg3.setAttribute('src', pratos[pratosUsados].img)
-  prato3.classList.add('prato')
-
-  pratoSpan3.appendChild(pratoNome3)
-  pratoSpan3.appendChild(pratoDesc3)
-  prato3.appendChild(pratoImg3)
-  prato3.appendChild(pratoSpan3)
-
-  boxPratos.appendChild(prato3)
-
-  pratosUsados = pratosUsados + 1
-  boxPratos.appendChild(spanRegua3)
-
-  // criandos as bases do carrossel
-
-  const boxM = document.createElement('div')
-
-  // --- editando as bases carrossel
-  boxM.classList.add('box-carrossel')
-
-  // --- add box carrossel
-  boxG.appendChild(boxM)
-
-  // criando as imagens do carrossel
-  const primeiraImg = document.createElement('img')
-  const segundaImg = document.createElement('img')
-  const terceiraImg = document.createElement('img')
-  const quartaImg = document.createElement('img')
-
-  // --- editando as imagens do carrossel
-
-  primeiraImg.setAttribute('src', pratos[imgCarrossel].img)
-  quartaImg.setAttribute('src', pratos[imgCarrossel].img)
-  imgCarrossel = imgCarrossel + 1
-
-  segundaImg.setAttribute('src', pratos[imgCarrossel].img)
-  imgCarrossel = imgCarrossel + 1
-
-  terceiraImg.setAttribute('src', pratos[imgCarrossel].img)
-  imgCarrossel = imgCarrossel + 1
-
-  boxM.appendChild(primeiraImg)
-  boxM.appendChild(segundaImg)
-  boxM.appendChild(terceiraImg)
-  boxM.appendChild(quartaImg)
-
   boxAdd = boxAdd + 1
+  boxVisiveis = boxVisiveis + 1
   ativarBtn()
+
+  document.getElementById('container-sobre').innerHTML =
+    document.getElementById('container-sobre').innerHTML +
+    `<div>
+      <img
+       src="../assets/images/divisor.png"
+       alt=""
+      class="divisor-icon"
+     />
+    </div>
+    <div class="container-margin teste">
+    <div class="box-pratos">
+      <div class="prato">
+        <img src="${pratos[pratosUsados].img}" alt="" />
+        <span>
+          <p class="nome">${pratos[pratosUsados].nome}</p>
+          <p class="desc">${pratos[pratosUsados].desc}
+          </p>
+        </span>
+      </div>
+      <span class="regua"></span>
+      <div class="prato">
+      <img src="${pratos[pratosUsados + 1].img}" alt="" />
+      <span>
+        <p class="nome">${pratos[pratosUsados + 1].nome}</p>
+        <p class="desc">${pratos[pratosUsados + 1].desc}
+        </p>
+      </span>
+    </div>
+      <span class="regua"></span>
+      <div class="prato">
+      <img src="${pratos[pratosUsados + 2].img}" alt="" />
+      <span>
+        <p class="nome">${pratos[pratosUsados + 2].nome}</p>
+        <p class="desc">${pratos[pratosUsados + 2].desc}
+        </p>
+      </span>
+    </div>
+      <span class="regua"></span>
+    </div>
+    <div class="carrossel-cardapio">
+      <img
+        src="assets-menu/foto-especialidades-carrossel-1.jpg"
+        alt=""
+      />
+    </div>
+  </div>`
+  pratosUsados = pratosUsados + 3
 }
 
 function ativarBtn() {
   var quantPratos = pratos.length / 3
   const verMenos = document.getElementById('ver-menos')
 
-  if (boxAdd == quantPratos) {
+  if (boxVisiveis == quantPratos) {
     document.getElementById('ver-mais').classList.add('sumir')
   }
 
-  if (boxAdd >= 1) {
+  if (boxVisiveis >= 1) {
     verMenos.classList.remove('sumir')
   }
+
+  if (boxVisiveis == 0) {
+    verMenos.classList.add('sumir')
+  }
+}
+
+function retirar() {
+  for (var i = boxAdd; i > 0; i = i - 1) {
+    if (
+      document.querySelectorAll('.sobremesas .container-margin')[i].style
+        .display == 'none'
+    ) {
+    } else {
+      document.querySelectorAll('.sobremesas .container-margin')[
+        i
+      ].style.display = 'none'
+      boxVisiveis = boxVisiveis - 1
+      i = 0
+    }
+  }
+  ativarBtn()
 }
